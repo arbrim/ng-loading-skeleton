cd3a368bf74d7cf922359ded9c7394060001f96b
arbrim <arbrimrashiti@gmail.com>
2025-03-15 01:45:18 +0100

    support primary and secondary color of the skeleton

--- projects/ng-loading-skeleton/src/lib/ng-loading-skeleton.component.ts
+++ projects/ng-loading-skeleton/src/lib/ng-loading-skeleton.component.ts
@@ -3,12 +3,19 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'ng-loading-skeleton',
   template: `
-    <div [ngStyle]="{ width: width, height: height, borderRadius: borderRadius }" class="skeleton"></div>
+    <div 
+      [ngStyle]="{
+        width: width, 
+        height: height, 
+        borderRadius: borderRadius, 
+        background: 'linear-gradient(90deg, ' + primaryColor + ' 25%, ' + secondaryColor + ' 50%, ' + primaryColor + ' 75%)',
+        backgroundSize: '400% 100%'
+      }" 
+      class="skeleton"
+    ></div>
   `,
   styles: [`
     .skeleton {
-      background: linear-gradient(90deg, #e0e0e0 25%, #f8f8f8 50%, #e0e0e0 75%);
-      background-size: 400% 100%;
       animation: shimmer 1.5s infinite;
     }
     @keyframes shimmer {
@@ -21,4 +28,6 @@ export class NgLoadingSkeletonComponent {
   @Input() width = '100%';
   @Input() height = '20px';
   @Input() borderRadius = '4px';
+  @Input() primaryColor = '#e0e0e0';
+  @Input() secondaryColor = '#f8f8f8';
 }
